05699d6419362121e6ee52f5d5af9fe1feaf6120
DreaMeR-17 <pronka_1993@mail.ru>
2024-08-09 13:03:52 +0600

    Update Program.cs
    RE:

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -9,46 +9,46 @@ namespace Dictionary
         {
             bool isWork = true;           
 
-            Dictionary<string, string> wordsAndMeanings = new Dictionary<string, string>();
+            Dictionary<string, string> words = new Dictionary<string, string>();
 
-            wordsAndMeanings.Add("Книга", "Бумажная продукцая, источник любой информации, например словарь или детектив.");
-            wordsAndMeanings.Add("Интернет", "Всемирная система объединенных сетей. В интернете можно найти юбую информацию в короткий срок.");
-            wordsAndMeanings.Add("Кот", "Существо, которое может жрать, бегать, спать.");
-            wordsAndMeanings.Add("Собака", "Лучший друг человека.");
-            wordsAndMeanings.Add("Печенька", "Еда, может использоваться как награда.");
-            wordsAndMeanings.Add("Программист", "Человек который быстро клацает по клавиатуре за печеньки.");
-            wordsAndMeanings.Add("ЯЮниор", "Лучшая школа IT");
+            words.Add("Книга", "Бумажная продукцая, источник любой информации, например словарь или детектив.");
+            words.Add("Интернет", "Всемирная система объединенных сетей. В интернете можно найти юбую информацию в короткий срок.");
+            words.Add("Кот", "Существо, которое может жрать, бегать, спать.");
+            words.Add("Собака", "Лучший друг человека.");
+            words.Add("Печенька", "Еда, может использоваться как награда.");
+            words.Add("Программист", "Человек который быстро клацает по клавиатуре за печеньки.");
+            words.Add("ЯЮниор", "Лучшая школа IT");
 
             Console.WriteLine("В нашем словаре на данный момент есть опредееления следующим словам: \n");          
 
             while (isWork)
             {
-                ShowAllWords(wordsAndMeanings);
+                ShowAllWords(words);
 
                 Console.WriteLine("\nВведите слово из словаря и мы выдадим его значение.");
 
-                TryGetMeanings(wordsAndMeanings);
+                TryFindWord(words);
             }
         }
 
-        static void ShowAllWords(Dictionary<string, string> wordsAndMeanings)
+        static void ShowAllWords(Dictionary<string, string> words)
         {
-            foreach (var word in wordsAndMeanings)
+            foreach (var word in words)
             {
                 Console.WriteLine($"{word.Key} ");
             }
         }
 
-        static void TryGetMeanings(Dictionary<string, string> wordsAndMeanings)
+        static void TryFindWord(Dictionary<string, string> words)
         {
             string userInput = Console.ReadLine();
 
             char space = '-';
 
-            if (wordsAndMeanings.ContainsKey(userInput))
+            if (words.ContainsKey(userInput))
             {
                 Console.Clear();
-                Console.WriteLine($"{userInput} {space} {wordsAndMeanings[userInput]}\n");
+                Console.WriteLine($"{userInput} {space} {words[userInput]}\n");
             }
             else
             {
